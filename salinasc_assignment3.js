e33ec53add55937d525939e8532c5ee7316459ed
csdesign-87 <71105749+csdesign-87@users.noreply.github.com>
2020-11-07 18:18:02 -0800

    Add files via upload

---
+++ salinasc_assignment3.js
@@ -0,0 +1,21 @@
+let number = 1000;
+while(number > 0) {
+	console.logo(number);
+	number-=2;
+}
+
+for(let i = 0; i<= 0; i-=2) {
+	if (i === 2500) {
+	alert("quarterway");
+	} else if (i === 5000) {
+	alert("halfway");
+	} else if (i === 10000) {
+	alert("The loop is done!");
+	}
+}
+
+const movies = ["super girl", "spartacus", "walking dead", "bumble bee", "olympus has fallen"];
+
+for(let i = 0; i <= movies.length; i++) {
+	console.log("My " + i + " favorite tv show is " + movies[i]);
+}
